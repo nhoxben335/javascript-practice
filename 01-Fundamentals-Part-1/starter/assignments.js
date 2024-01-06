fde32971bb0b821c1aa996c6bacf53e724c1476f
Nhan Vo <nhoxben335@gmail.com>
2024-01-06 19:49:33 -0400

    finished values and variables assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,13 @@
+// ASSIGNMENTS
+
+// LECTURE: Values and Variables
+
+// 1. Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+var country = "Halifax";
+var continent = "Nova Scotia";
+var population = "423,000";
+
+console.log(country);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,8 @@
-let js = "amazing";
-if (js === "amazing") alert("Javascript is FUN");
+// let js = "amazing";
+// if (js === "amazing") alert("Javascript is FUN");
 
-console.log(40 + 8 + 23 - 10);
-console.log("Jonas");
-console.log(23);
+// console.log(40 + 8 + 23 - 10);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas";
-
-
+// let firstName = "Jonas";
