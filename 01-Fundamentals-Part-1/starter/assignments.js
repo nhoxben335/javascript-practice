fa9afb3f2928370eabb2a4bf03e4a166c93344a3
Nhan Vo <nhoxben335@gmail.com>
2024-01-07 17:05:30 -0400

    data types assignment done

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -10,4 +10,18 @@ var country = "Halifax";
 var continent = "Nova Scotia";
 var population = "423,000";
 
-console.log(country);
+// LECTURE: Data Types
+
+// 1. Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
